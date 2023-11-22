c7a2434f5d3f4f343f1f92cff46092cdd376af45
Bre Aydin <blaydin@usc.edu>
2023-11-21 20:14:58 -0800

    Update UserCoursesServlet.java

--- src/main/java/studyconnect/UserCoursesServlet.java
+++ src/main/java/studyconnect/UserCoursesServlet.java
@@ -26,7 +26,7 @@ public class UserCoursesServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TO-DO: Add user's courses to Course database table
 	}
 
 	/**
